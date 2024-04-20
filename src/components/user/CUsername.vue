68f39150ece902db2376d7b81d9a5a491c0fc036
DWaveletT <dingwentao2004@126.com>
2024-04-20 12:14:16 +0800

    增加管理员 tag

--- src/components/user/CUsername.vue
+++ src/components/user/CUsername.vue
@@ -1,9 +1,11 @@
 <template>
-    <span>{{ prop.user.name }}</span>
+    <span>{{ prop.user.name }}<el-tag v-if="prop.user.role === 2" type="success" class="admin">管理</el-tag></span>
 </template>
 
 <script setup lang="ts">
 import type { User } from '@/interface';
+
+import { ElTag } from 'element-plus';
 
 const prop = defineProps<{
     user: User
@@ -12,5 +14,7 @@ const prop = defineProps<{
 </script>
 
 <style scoped lang="scss">
-
+.admin {
+    margin-left: 0.5em;
+}
 </style>
